06cdca066c3c47a6076c1501b395e49645124bc0
Liam <lmansfield928@gmail.com>
2024-11-10 21:14:39 -0500

    Homescreen(Patient list) commit

---
+++ app/src/main/java/com/example/firebase/HomeScreen.java
@@ -0,0 +1,19 @@
+package com.example.firebase;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeScreen extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homescreen);
+
+
+
+    }
+}
