967920a2ebb909ab6e7130421f38514cf4db8508
Liam <lmansfield928@gmail.com>
2024-11-10 22:04:50 -0500

    v2 homescreen and connected devices

--- app/src/main/java/com/example/firebase/HomeScreen.java
+++ app/src/main/java/com/example/firebase/HomeScreen.java
@@ -1,17 +1,20 @@
 package com.example.firebase;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeScreen extends AppCompatActivity {
 
+    private ListView patientList;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
+        patientList = findViewById(R.id.patientList);
 
 
 
